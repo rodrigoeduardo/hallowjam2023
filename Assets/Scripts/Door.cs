bf45a942f11d470f8e8f1ba5aa1b5203f12fd592
Alan Sol Alves Dias <alanlua2020@hotmail.com>
2023-10-15 14:26:10 -0300

    level 2...

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -28,7 +28,7 @@ public class Door : MonoBehaviour
         else direction = 1;
         distance = Vector2.Distance(startPosition, transform.position);
 
-        if((distance < maxDistance && direction == -1) || (distance > 0.1f && direction == 1))
+        if((distance < maxDistance && direction == -1) || (distance > 0.05f && direction == 1))
         {
 
             transform.Translate(new Vector3(0, Time.deltaTime * speed * direction));
