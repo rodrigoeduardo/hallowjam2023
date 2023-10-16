6463a45568ca10e10dc252176ae62e1bd3daf7b2
Ezequiel Morais <ezequielmorais4002@gmail.com>
2023-10-15 21:44:10 -0300

    Criado emissor de eventos na fase

---
+++ Assets/Scripts/FaseEventEmmiter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaseEventEmmiter : MonoBehaviour
+{
+    public string[] dialog; /* ARMAZENA AS FALAS DO DIÁLOGO DESSE EVENTO */
+    public int index; /* ARMAZENA O ÍNDICE ATUAL DA FALA NO DIÁLOGO */
+    private bool hasNoiteArrived; /* ARMAZENA SE NOITE JÁ CHEGOU NO EVENTO */
+    private bool hasBlairArrived; /* ARMAZENA SE BLAIR JÁ CHEGOU NO EVENTO */
+    private bool hasPlayed; /* ARMAZENA SE O EVENTO JÁ FOI TOCADO OUTRA VEZ */
+    // Start is called before the first frame update
+    void Start()
+    {
+        hasNoiteArrived = hasBlairArrived = hasPlayed = false;
+        index = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
